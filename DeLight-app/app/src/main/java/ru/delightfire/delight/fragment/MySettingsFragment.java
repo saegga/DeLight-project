1168d2a3b7fb85a3254fc4c194f4406281fad8dc
saegga <sergei.muzalev94@mail.ru>
2015-11-12 13:39:52 -1200

    MysettingsFragment

---
+++ DeLight-app/app/src/main/java/ru/delightfire/delight/fragment/MySettingsFragment.java
@@ -0,0 +1,24 @@
+package ru.delightfire.delight.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ru.delightfire.delight.R;
+
+/**
+ * Created by sergei on 12.11.2015.
+ */
+public class MySettingsFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_my_settings, container, false);
+        return view;
+    }
+}

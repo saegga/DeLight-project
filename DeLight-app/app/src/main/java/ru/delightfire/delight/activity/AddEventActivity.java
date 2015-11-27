b61b2b3e1a5192a5338a8c3bc109f26013a1a5de
saegga <sergei.muzalev94@mail.ru>
2015-11-27 00:27:56 -1200

    add save state date

--- DeLight-app/app/src/main/java/ru/delightfire/delight/activity/AddEventActivity.java
+++ DeLight-app/app/src/main/java/ru/delightfire/delight/activity/AddEventActivity.java
@@ -1,6 +1,7 @@
 package ru.delightfire.delight.activity;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
@@ -16,6 +17,7 @@ import ru.delightfire.delight.fragment.AddShowFragment;
  * Created by sergei on 18.11.2015.
  */
 public class AddEventActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener {
+    public static final String FRAGMENT_ADD_SAVE_STATE = "fragment_add_save_state";
     private RadioGroup group;
     private RadioButton btnPerformance;
     private RadioButton btnMeet;
@@ -24,6 +26,7 @@ public class AddEventActivity extends AppCompatActivity implements RadioGroup.On
     private FragmentTransaction transaction;
     private AddShowFragment performanceFragment;
     private AddMeetFragment meetFragment;
+    Fragment actionFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,8 +37,7 @@ public class AddEventActivity extends AppCompatActivity implements RadioGroup.On
         btnTraining = (RadioButton) findViewById(R.id.btn_training);
         group.setOnCheckedChangeListener(this);
         manager = getSupportFragmentManager();
-        performanceFragment = new AddShowFragment();
-        meetFragment = new AddMeetFragment();
+
 
     }
 
@@ -44,26 +46,53 @@ public class AddEventActivity extends AppCompatActivity implements RadioGroup.On
         transaction = manager.beginTransaction();
         switch (checkedId){
             case  R.id.btn_performance :
+                actionFragment = new AddShowFragment();
                 if(manager.getFragments() != null){
                     Log.d("AddEventActivity", "replace");
-                    transaction.replace(R.id.add_event_container , performanceFragment);
+
+                    transaction.replace(R.id.add_event_container , actionFragment);
                 }else{
                     Log.d("AddEventActivity", "add");
-                    transaction.add(R.id.add_event_container , performanceFragment);
+                    transaction.add(R.id.add_event_container , actionFragment);
                 }
                 break;
             case  R.id.btn_meet :
+                actionFragment = new AddMeetFragment();
                 if(manager.getFragments() != null){
                     Log.d("AddEventActivity", "replace");
-                    transaction.replace(R.id.add_event_container , meetFragment);
+                    transaction.replace(R.id.add_event_container , actionFragment);
                 }else{
                     Log.d("AddEventActivity", "add");
-                    transaction.add(R.id.add_event_container , meetFragment);
+                    transaction.add(R.id.add_event_container , actionFragment);
                 }
                 Log.d("AddEventActivity", "check" + checkedId );
                 break;
             case  R.id.btn_training : Log.d("AddEventActivity", "check" + checkedId);break;
         }
         transaction.commit();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        if(actionFragment != null){
+            manager.putFragment(outState, FRAGMENT_ADD_SAVE_STATE, actionFragment);
+        }
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        //super.onRestoreInstanceState(savedInstanceState);
+        actionFragment = manager.getFragment(savedInstanceState, FRAGMENT_ADD_SAVE_STATE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(actionFragment != null){
+            transaction = manager.beginTransaction();
+            transaction.replace(R.id.add_event_container , actionFragment);
+            transaction.commit();
+        }
     }
 }

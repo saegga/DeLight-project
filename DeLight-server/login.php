afdc00ff4247a57c6c959f87e0fde162b50baef4
scaredChatsky <scrd.cha@gmail.com>
2015-12-07 15:42:17 +0900

    tubular

--- DeLight-server/login.php
+++ DeLight-server/login.php
@@ -33,11 +33,14 @@ header('Content-type: text/html; charset=utf-8');
 	</div>
 	
 	<script src="libs/jquery/jquery-2.1.4.min.js"></script>
+	<script type="text/javascript" charset="utf-8" src="libs/jquery.tubular.1.0.1/js/jquery.tubular.1.0.js"></script> 
 
 	<script>
 	$(document).ready(function() {
 
 		$(".main").css("height", $(window).height());
+
+        $('.main').tubular({videoId: 'k5kmjb0Eq1Y'});
 
 	});
 	</script>

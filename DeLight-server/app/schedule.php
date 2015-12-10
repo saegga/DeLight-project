ebd7a2b77aed35b30c7be4fe4d406a67cf55c0c0
scaredChatsky <scrd.cha@gmail.com>
2015-12-11 00:09:05 +0900

    bug fix with password hash

--- DeLight-server/app/schedule.php
+++ DeLight-server/app/schedule.php
@@ -4,14 +4,14 @@
 	<div class="centered user-area-wrapper">
 		<div class="user-area">
 			<div class="menu violet-style">
-				<div class="tab-button">
+				<a class="tab-button" href="#schedule">
 					<span class="icon-basic-todo-txt"></span>
 					<span class="menu-item">Расписание</span>
-				</div>
-				<div class="tab-button">
+				</a>
+				<a class="tab-button" href="#logout">
 					<span class="menu-item">Выход</span>
 					<span class="fa fa-sign-out"></span>
-				</div>
+				</a>
 			</div>
 			<div class="schedule-container violet-style">
 				<?php include "include/events.php" ?>
